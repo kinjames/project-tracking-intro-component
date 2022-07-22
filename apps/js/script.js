bdd41db041c5b57be7602821470a90b8c633f01f
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-07-22 21:54:06 +0000

    Add files via upload

--- apps/js/script.js
+++ apps/js/script.js
@@ -3,9 +3,13 @@ const mobileMenu = document.querySelector(".mobile-menu")
 const header = document.querySelector(".header")
 
 menuBtns.addEventListener('click', function() {
-    if (mobileMenu.classList.contains('open')){
-        mobileMenu.classList.remove('open')
-    } else {
+    if (mobileMenu.classList.contains('hide')){
+        menuBtns.classList.add('burger')
+        mobileMenu.classList.remove('hide')
         mobileMenu.classList.add('open')
+    } else {
+        mobileMenu.classList.remove('open')
+        mobileMenu.classList.add('hide')
+        menuBtns.classList.remove('burger')
     }
 })
